f3dd80db02686c79e8897f6e674c177f4236a292
neMagrel <markokcat@yandex.ru>
2026-09-22 21:54:47 +0500

    выполнил 4 задание

--- src/task4-parser.ts
+++ src/task4-parser.ts
@@ -7,7 +7,9 @@ export type Transaction = {
 
 // 1. Напишите предикат isTransaction (можно скопировать из task2)
 export function isTransaction(data: unknown): data is Transaction {
-  // ...
+  return data instanceof Object && data !== null
+   && "id" in data && "amount" in data && "type" in data 
+   && typeof data.id === "string" && typeof data.amount === "number" &&  (data.type === "deposit" || data.type === "withdrawal")
 }
 
 // 2. Напишите функцию parseTransactions
@@ -16,12 +18,35 @@ export function isTransaction(data: unknown): data is Transaction {
 // Логика: пройтись по массиву. Если isTransaction(item) - добавить в valid.
 // Иначе - добавить строку "Invalid item: <item>" в errors.
 export function parseTransactions(rawData: unknown[]): { valid: Transaction[]; errors: string[] } {
-  // Напишите код здесь
+  const valid: Transaction[] = []
+  const errors: string[] = []
+
+  for (const item of rawData) {
+    if (isTransaction(item)) {
+      valid.push(item)
+    }
+    else {
+      errors.push("Invalid item: " + item)
+    }
+  }
+
+  return { valid, errors }
 }
 
 // 3. Напишите функцию calculateBalance
 // Принимает массив валидных транзакций.
 // deposit прибавляет amount, withdrawal вычитает.
 export function calculateBalance(transactions: Transaction[]): number {
-  // Напишите код здесь
+  let balance = 0
+
+  for (const transaction of transactions) {
+    if (transaction.type === "deposit") {
+      balance = balance + transaction.amount
+    }
+    else {
+      balance = balance - transaction.amount
+    }
+  }
+
+  return balance
 }
